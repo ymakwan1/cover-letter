e44512bd8f4e6b9803303010226ea768bbb6d76c
ymakwan1 <ymakwan1@binghamton.edu>
2024-05-18 10:42:12 +0530

    bash file generted for alias

---
+++ generate_cover_letter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/yashmakwana/Desktop/Projects/cover-letter
+source /Users/yashmakwana/Desktop/Projects/cover-letter/venv/bin/activate
+python3 cover_letter_generator.py "$1" "$2"
+
