ca70edced570dd2d180b1848cbcb7a3857864128
ymakwan1 <ymakwan1@binghamton.edu>
2024-05-18 10:06:38 +0530

    fixed output path

--- cover_letter_generator.py
+++ cover_letter_generator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from docx import Document
 from docx.shared import Pt
 from dotenv import load_dotenv
@@ -11,9 +12,14 @@ def generate_cover_letter(position: str, company: str) -> None:
     template_path = os.getenv('COVER_LETTER_TEMPLATE_PATH')
     font_size_str = os.getenv('COVER_LETTER_FONT_SIZE')
     font_name = os.getenv('COVER_LETTER_FONT_NAME')
+    output_dir = os.getenv('COVER_LETTER_OUTPUT_DIR')
 
     if not template_path:
         print("Cover letter template path not found. Please set the COVER_LETTER_TEMPLATE_PATH environment variable.")
+        return
+
+    if not output_dir:
+        print("Output directory path not found. Please set the COVER_LETTER_OUTPUT_DIR environment variable.")
         return
 
     # Convert font size to integer
@@ -38,21 +44,27 @@ def generate_cover_letter(position: str, company: str) -> None:
                 run.font.name = font_name
 
     # Define the output directory if it doesn't exist
-    output_dir = 'output'
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Save the modified document as DOC
+    # Save the modified document as DOCX
     doc_path = os.path.join(output_dir, f"cover_letter_{company}_{position}.docx")
     doc.save(doc_path)
 
     # Convert the Word document to PDF
-    pdf_path = os.path.join(output_dir, f"cover_letter_{company}_{position}.pdf")
+    pdf_path = os.path.join(output_dir, f"Yash_Makwana_Cover_Letter_{company}_{position}.pdf")
     convert(doc_path, pdf_path)
 
     print(f"Cover letter generated and saved as: {pdf_path}")
 
+    # Delete the DOCX file
+    os.remove(doc_path)
+
 if __name__ == "__main__":
-    position: str = input("Enter the desired position for the cover letter: ")
-    company: str = input("Enter the name of the company: ")
+    if len(sys.argv) != 3:
+        print("Usage: python3 cover_letter_generator.py <position> <company>")
+        sys.exit(1)
+
+    position = sys.argv[1]
+    company = sys.argv[2]
     generate_cover_letter(position, company)
